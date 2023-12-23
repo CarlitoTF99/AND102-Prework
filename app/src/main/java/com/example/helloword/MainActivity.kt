da96ea0edb3aaa8dd4c790f3ec27fc6ef2f8ed45
CarlitoTF99 <huertero.jose654@gmail.com>
2023-12-23 13:13:23 -0500

    first commit

---
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloword
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var startButton = findViewById<Button>(R.id.helloword_button)
+
+        startButton.setOnClickListener(View.OnClickListener { // Show a Toast when the button is clicked
+            Toast.makeText(this@MainActivity, "Hello Word!!", Toast.LENGTH_SHORT).show()
+        })
+    }
+}
